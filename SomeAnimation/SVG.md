508bff357b55675ace07e1993c089c43982e4efb
Gopal soni <Gopals.btech2022@iujaipur.edu.in>
2024-06-11 15:02:28 +0530

    moving towards SVG implementation

---
+++ SomeAnimation/SVG.md
@@ -0,0 +1,11 @@
+ Learning about SVG and why they used
+
+ 1) SVG => SCALABLE VECTOR GRAPHICS
+
+ Pros of SVG 
+   => 1) Scalable
+      
+
+      Learning about its properties in code 
+
+      <svg width='190' ><svg>
--- SomeAnimation/index.js
+++ SomeAnimation/index.js
@@ -1,1 +1,12 @@
-gsap.to("")
+gsap.to("#page2 h1",{
+    transform:'translateX(-150%)',
+    scrollTrigger:{
+        trigger:'#page2 h1',
+        scroller:"body",
+        start:'top 0%',
+        end:'top -100%',
+        markers:true,
+        scrub:2,
+        pin:true
+    }
+})
